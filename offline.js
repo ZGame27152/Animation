7ab7a56e375f215439c97c618b7bb91c0c22ad15
ZGame27152 <apipu.game@gmail.com>
2019-06-27 14:27:15 +0700

    Animation
    Animation

---
+++ offline.js
@@ -0,0 +1,11 @@
+﻿{
+	"version": 1561620316,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/animation-sheet0.png",
+		"images/animation-sheet1.png"
+	]
+}
